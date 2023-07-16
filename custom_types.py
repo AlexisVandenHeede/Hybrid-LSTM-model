65b3c5c4cc022d6aadbc6aeffca1e94efa4b7ee5
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-07-16 22:38:23 +1000

    Add type aliases for model and battery parameters.

---
+++ custom_types.py
@@ -0,0 +1,5 @@
+from typing import Literal, TypeAlias
+
+
+Model: TypeAlias = Literal["data", "hybrid"]
+Battery: TypeAlias = Literal["B0005", "B0006", "B0007", "B0018", "B0029", "B0030", "B0031", "B0032"]
