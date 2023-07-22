acc6f3dd0df7383f3ae5a6e9b1477a31d8966458
gowrimenon2005 <GowriRemeshMenon@student.tudelft.nl>
2023-07-22 14:05:19 +0200

    main parts done

--- EKFalgorithm.py
+++ EKFalgorithm.py
@@ -48,7 +48,7 @@ class ECM():
         indx = np.array(indx)
         return indx
 
-    def EKF(self, with_discharge_cycles=False, save_plot=False):
+    def EKF(self, save_plot=False, save_data=False):
         # Initial conditions
         soc_init = 1
         X = np.matrix([[soc_init], [0], [0]])
@@ -68,101 +68,23 @@ class ECM():
         Q_x = np.matrix([[self.q1, 0, 0], [0, self.q2, 0], [0, 0, self.q3]])
 
         # EKF
-        if with_discharge_cycles:
-            idx = self.get_discharge_cycles()
-            total_err = 0
-            for i in range(len(idx)-1):
-                SOC_est = []
-                Vt_est = []
-                Vt_err = []
-                temperature = self.bat['Temperature_measured'][idx[i]+1:idx[i+1]-1]
-                current = self.bat['Current_measured'][idx[i]+1:idx[i+1]-1]
-                Vt_act = self.bat['Voltage_measured'][idx[i]+1:idx[i+1]-1]
-                indx = []
-                Vt_act_plot = []
-
-                for k in range(len(current)):
-                    T_val = temperature[k+idx[i]+1]
-                    U = current[k+idx[i]+1]
-                    soc = X.item(0)
-                    V1 = X[1]
-                    V2 = X[2]
-
-                    R0 = s.interpolate.griddata((self.SOC_dat, self.T_dat), self.R0_dat, (soc, T_val), method='nearest')
-                    R1 = s.interpolate.griddata((self.SOC_dat, self.T_dat), self.R1_dat, (soc, T_val), method='nearest')
-                    R2 = s.interpolate.griddata((self.SOC_dat, self.T_dat), self.R2_dat, (soc, T_val), method='nearest')
-                    C1 = s.interpolate.griddata((self.SOC_dat, self.T_dat), self.C1_dat, (soc, T_val), method='nearest')
-                    C2 = s.interpolate.griddata((self.SOC_dat, self.T_dat), self.C2_dat, (soc, T_val), method='nearest')
-
-                    ocv_pred = sococv(np.array(soc))
-
-                    tau1 = R1*C1
-                    tau2 = R2*C2
-
-                    a1 = np.exp(-deltaT/tau1).item(0)
-                    a2 = np.exp(-deltaT/tau2).item(0)
-
-                    b1 = (R1 * (1-a1)).item(0)
-                    b2 = (R2 * (1-a2)).item(0)
-
-                    terminal_voltage = ocv_pred - R0 * U - V1 - V2
-
-                    dOCV = dsococv(soc)
-
-                    C_x = np.matrix([dOCV, -1, -1])
-
-                    Error_x = Vt_act[k+idx[i]+1] - terminal_voltage
-
-                    Vt_est.append(terminal_voltage.item(0))
-                    Vt_err.append(Error_x.item(0))
-                    SOC_est.append(soc)
-                    indx.append(k+idx[i]+2)
-                    Vt_act_plot.append(Vt_act[k+idx[i]+1])
-
-                    # Prediction
-                    A = np.matrix([[1, 0, 0], [0, a1, 0], [0, 0, a2]])
-                    B = np.matrix([[(-deltaT/Qn_rated)], [b1], [b2]])
-                    X = A*X + B*U
-                    P_x = A*P_x*A.T + Q_x
-                    Kalman_gain = P_x*C_x.T*(C_x*P_x*C_x.T + R_x).I
-                    X = X + Kalman_gain*Error_x
-                    P_x = (np.eye(3) - Kalman_gain*C_x)*P_x
-                
-                # remove extra data points
-                Vt_est = Vt_est[10:]
-                Vt_err = Vt_err[10:]
-                SOC_est = SOC_est[10:]
-                indx = indx[10:]
-                Vt_act_plot = Vt_act_plot[10:]
-                total_err += np.sum(np.square(Vt_err))
-
-                if save_plot:
-                    # print(Vt_est)
-                    plt.figure(1)
-                    plt.plot(indx, Vt_act_plot, 'r')
-                    plt.plot(indx, Vt_est, 'b')
-                    plt.xlabel('Instance')
-                    plt.ylabel('Terminal voltage')
-                    plt.figure(2)
-                    plt.plot(indx, Vt_err, 'g')
-                    plt.xlabel('Instance')
-                    plt.ylabel('Absolute error')
-                    plt.figure(3)
-                    plt.plot(indx, SOC_est, 'b')
-                    plt.xlabel('Instance')
-                    plt.ylabel('SOC')
-
-            plt.show()
-            total_err = total_err/(len(idx)-1)
-            print(f'MSE is {total_err}')
-            return SOC_est, Vt_est, Vt_err, total_err
-        else:
+        idx = self.get_discharge_cycles()
+        total_err = 0
+        self.main_data_soc = []
+        self.main_data_vt_est = []
+        for i in range(len(idx)-1):
             SOC_est = []
             Vt_est = []
             Vt_err = []
+            temperature = self.bat['Temperature_measured'][idx[i]+1:idx[i+1]-1]
+            current = self.bat['Current_measured'][idx[i]+1:idx[i+1]-1]
+            Vt_act = self.bat['Voltage_measured'][idx[i]+1:idx[i+1]-1]
+            indx = []
+            Vt_act_plot = []
+
             for k in range(len(current)):
-                T_val = temperature[k]
-                U = current[k]
+                T_val = temperature[k+idx[i]+1]
+                U = current[k+idx[i]+1]
                 soc = X.item(0)
                 V1 = X[1]
                 V2 = X[2]
@@ -174,27 +96,22 @@ class ECM():
                 C2 = s.interpolate.griddata((self.SOC_dat, self.T_dat), self.C2_dat, (soc, T_val), method='nearest')
 
                 ocv_pred = sococv(np.array(soc))
-
                 tau1 = R1*C1
                 tau2 = R2*C2
-
                 a1 = np.exp(-deltaT/tau1).item(0)
                 a2 = np.exp(-deltaT/tau2).item(0)
-
                 b1 = (R1 * (1-a1)).item(0)
                 b2 = (R2 * (1-a2)).item(0)
-
                 terminal_voltage = ocv_pred - R0 * U - V1 - V2
-
                 dOCV = dsococv(soc)
-
                 C_x = np.matrix([dOCV, -1, -1])
-
-                Error_x = Vt_act[k] - terminal_voltage
+                Error_x = Vt_act[k+idx[i]+1] - terminal_voltage
 
                 Vt_est.append(terminal_voltage.item(0))
                 Vt_err.append(Error_x.item(0))
                 SOC_est.append(soc)
+                indx.append(k+idx[i]+2)
+                Vt_act_plot.append(Vt_act[k+idx[i]+1])
 
                 # Prediction
                 A = np.matrix([[1, 0, 0], [0, a1, 0], [0, 0, a2]])
@@ -204,32 +121,54 @@ class ECM():
                 Kalman_gain = P_x*C_x.T*(C_x*P_x*C_x.T + R_x).I
                 X = X + Kalman_gain*Error_x
                 P_x = (np.eye(3) - Kalman_gain*C_x)*P_x
+            
+            # remove extra data points
+            Vt_est = Vt_est[10:]
+            Vt_err = Vt_err[10:]
+            SOC_est = SOC_est[10:]
+            indx = indx[10:]
+            Vt_act_plot = Vt_act_plot[10:]
+            total_err += np.sum(np.square(Vt_err))
 
-            total_err = (np.sum(np.square(Vt_err)))/len(Vt_err)
-            print(f'MSE is {total_err}')
             if save_plot:
+                # print(Vt_est)
                 plt.figure(1)
-                plt.plot(Vt_act, 'r')
-                plt.plot(Vt_est, 'b')
+                plt.plot(indx, Vt_act_plot, 'r')
+                plt.plot(indx, Vt_est, 'b')
                 plt.xlabel('Instance')
                 plt.ylabel('Terminal voltage')
                 plt.figure(2)
-                plt.plot(Vt_err, 'g')
+                plt.plot(indx, Vt_err, 'g')
                 plt.xlabel('Instance')
                 plt.ylabel('Absolute error')
                 plt.figure(3)
-                plt.plot(SOC_est, 'b')
+                plt.plot(indx, SOC_est, 'b')
                 plt.xlabel('Instance')
                 plt.ylabel('SOC')
-                plt.show()
-            return SOC_est, Vt_est, Vt_err, total_err
+            
+            if save_data:
+                for p in range(len(SOC_est)):
+                    self.main_data_soc.append(SOC_est[p])
+                    self.main_data_vt_est.append(Vt_est[p])
+
+        plt.show()
+        total_err = total_err/(len(idx)-1)
+        print(f'MSE is {total_err}')
+        return SOC_est, Vt_est, Vt_err, total_err
+    
+    def save_data(self):
+        df_soc = pd.DataFrame(self.main_data_soc)
+        df_soc.to_csv(f'ECM/soc_est_{self.battery_num}.csv')
+        df_vt = pd.DataFrame(self.main_data_vt_est)
+        df_vt.to_csv(f'ECM/vt_est_{self.battery_num}.csv')
 
 
 # testing if the algorithm works
 # please uncomment this if you want to run the optimiser
 # ecm = ECM(0.25098788, 0.3372615, 0.003931529, 0.819609, 0.003931529, 0.8196096, 0.64706235, 'B0005')  # higher MSE but clear rul degredation wo. discharge cycles
-# # ecm = ECM(3.2156930588235295, 5.098044117647058, 0.4313821176470588, 9.490196588235294, 0.4313821176470588, 9.490196588235294, 8.35294282352941, 'B0005')  # Weird spiking behaviour no real rul degredation wo. discharge cycles
-ecm = ECM(6.8235325882352935, 7.843139411764706, 1.1372637647058823, 6.980395176470588, 1.1372637647058823, 6.980395176470588, 9.294118352941176, 'B0005') # ga opt values w. discharge cycles
-ecm = ECM(r=2.078439294117647, p1=8.901961882352941, p2=0.11765694117647059, p3=4.23529988235294, q1=0.11765694117647059, q2=4.23529988235294, q3=9.137255764705882, battery_num='B0005')
-ecm = ECM(r=7.647061176470587, p1=8.745099294117647, p2=2.4313801176470586, p3=6.862748235294117, q1=2.4313801176470586, q2=6.862748235294117, q3=7.607845529411764, battery_num='B0006')
-soc_est, vt_est, vt_err, total_err = ecm.EKF(with_discharge_cycles=True, save_plot=True)
+# # # ecm = ECM(3.2156930588235295, 5.098044117647058, 0.4313821176470588, 9.490196588235294, 0.4313821176470588, 9.490196588235294, 8.35294282352941, 'B0005')  # Weird spiking behaviour no real rul degredation wo. discharge cycles
+ecm = ECM(988.3529, 88.628588, 96.0788235, 40.39811, 96.0788235, 40.39811, 0.40211, 'B0005')  # ga opt values w. discharge cycles
+# ecm = ECM(r=2.078439294117647, p1=8.901961882352941, p2=0.11765694117647059, p3=4.23529988235294, q1=0.11765694117647059, q2=4.23529988235294, q3=9.137255764705882, battery_num='B0005')
+# ecm = ECM(r=7.647061176470587, p1=8.745099294117647, p2=2.4313801176470586, p3=6.862748235294117, q1=2.4313801176470586, q2=6.862748235294117, q3=7.607845529411764, battery_num='B0006')
+soc_est, vt_est, vt_err, total_err = ecm.EKF(save_plot=True, save_data=True)
+ecm.save_data()
